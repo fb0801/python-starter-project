d1704c984de486f13f81fb14921a142b1a1e0d64
fb0801 <74499472+fb0801@users.noreply.github.com>
2021-08-24 18:38:01 +0100

    Add files via upload

--- password_manager.py
+++ password_manager.py
@@ -1,5 +1,24 @@
-master_pwd =input("What is the master password? ")
+from cryptography.fernet import Fernet
 
+
+
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)'''
+
+def load_key():
+    file =open("key.key", "rb")
+    key=file.read()
+    file.close()
+    return key
+
+
+key = load_key()
+
+master_pwd =input("What is the master password? ")
 
 def view():
      with open("passwords.txt", "r") as f:
