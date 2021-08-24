6f6a2baedd7b7dd449bdbeebea1a27d0a656847f
fb0801 <74499472+fb0801@users.noreply.github.com>
2021-08-24 17:32:01 +0100

    Add files via upload

--- choose-your-adventure-game.py
+++ choose-your-adventure-game.py
@@ -6,19 +6,19 @@ answer = input(
     "You are on a dirt road, it has come to an end and you can go left or right")
 
 if answer =="left":
-    answer = input("You come across a river and can walk around to swin across? Type walk to walk around or swim to swim around")
+    answer = input("You come across a river and can walk around to swin across? Type walk to walk around or swim to swim around").lower()
 
     if answer =="swim":
-        print("")
+        print("you swam across and were eaten by an alligator.")
     elif answer =="walk":
-        print("")
+        print("you walked for many miles, ran out of food and water")
 
     else:
         print("not a valid option. your quest has ended")
         
     
 elif answer =="right":
-    print()
+    answer = input("You stumble across a bridge, you want to cross it or head back")
 
 else:
     print("Not a valid option. Your quest has ended")
