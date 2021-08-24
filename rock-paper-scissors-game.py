fbc49dca830e0ebc338afacfb2b710956c262791
fb0801 <74499472+fb0801@users.noreply.github.com>
2021-08-24 15:39:43 +0100

    Add files via upload

---
+++ rock-paper-scissors-game.py
@@ -0,0 +1,17 @@
+import random
+
+
+user_wins = 0
+computer_wins = 0
+
+while True:
+    user_input =input("Type Rock/Paper/Scissors or Q to qit:").lower()
+    if user_input =="q":
+        break #end the program
+
+    if user_input not in ["rock","paper","scissors"]:
+        continue
+
+
+
+print("This is the end \n This is the end \n Beautiful friend \n This is the end \n My only friend, the end")
