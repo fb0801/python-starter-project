c661c7154fc0e26c498648ddb821a5fd1e7b9889
fb0801 <74499472+fb0801@users.noreply.github.com>
2021-08-24 18:18:17 +0100

    Add files via upload

--- password_manager.py
+++ password_manager.py
@@ -1,3 +1,22 @@
 pwd =input("What is the master password? ")
 
-mode = input("add new pwd")
+
+def view():
+    pass
+
+def add():
+    pass
+
+while True:
+    mode = input("add new pwd or view exisitn ones (view or addd)").lower()
+
+    if mode =="view":
+        pass
+
+    elif mode=="add":
+        pass
+    elif mode=="q":
+        break
+
+    else:
+        print("Invalid option")
