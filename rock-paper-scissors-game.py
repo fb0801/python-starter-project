6f0053c8dcb2e6edfbe3950526f9f927040e0fd3
fb0801 <74499472+fb0801@users.noreply.github.com>
2021-08-24 15:52:13 +0100

    Add files via upload

--- rock-paper-scissors-game.py
+++ rock-paper-scissors-game.py
@@ -4,14 +4,40 @@ import random
 user_wins = 0
 computer_wins = 0
 
+options = ["rock","paper","scissors"]
+
 while True:
     user_input =input("Type Rock/Paper/Scissors or Q to qit:").lower()
     if user_input =="q":
         break #end the program
 
-    if user_input not in ["rock","paper","scissors"]:
+    if user_input not in options:
         continue
 
 
+    random_number = random.randint(0,2)
+    # rock0, paper: 1, sci: 2
+    computer_pick = options[random_number]
+    print("Computer picked", computer_pick)
 
+
+    if user_input =="rock" and computer_pick =="scissors":
+        print("you won!")
+        user_wins+=1
+        
+    elif user_input =="paper" and computer_pick =="rock":
+        print("you won!")
+        user_wins+=1
+        
+    elif user_input =="rock" and computer_pick =="paper":
+        print("you won!")
+        user_wins+=1
+        
+    else:
+        print("You lost I win human")
+        computer_wins +=1
+    
+
+
+    
 print("This is the end \n This is the end \n Beautiful friend \n This is the end \n My only friend, the end")
